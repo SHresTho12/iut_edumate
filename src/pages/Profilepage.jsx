b27c4730e20fc817bbde585419f0e492949d60a0
SHresTho12 <talimul12bari@gmail.com>
2022-09-04 02:26:54 +0600

    Profile page current user condition added. That means the profile page shows informnation if the user is logged in

--- src/pages/Profilepage.jsx
+++ src/pages/Profilepage.jsx
@@ -1,19 +1,19 @@
+import { chakra, Container, Heading } from '@chakra-ui/react'
 import React from 'react'
 import { Layout } from '../components/Layout'
-import { Badge, Code, Container, Heading } from '@chakra-ui/react'
-import { Card } from '../components/Card'
+import { useAuth } from '../Context/AuthContext'
 
 export default function Profilepage() {
+  const { currentUSer } = useAuth()
+  console.log(currentUSer)
   return (
     <Layout>
-      <Heading>
-        Profile page
-        <Badge colorScheme='green' fontSize='lg' mx={4}>
-          Protected Page
-        </Badge>
-      </Heading>
-
-      <Container maxW='container.lg' overflowX='auto' py={4}></Container>
+      <Heading>Profile page</Heading>
+      <Container maxW='container.lg' overflowX='auto' py={4}>
+      <chakra.pre p={4}>
+          {currentUSer && <pre> {JSON.stringify(currentUSer, null, 2)}</pre>}
+          </chakra.pre>
+      </Container>
     </Layout>
   )
 }
