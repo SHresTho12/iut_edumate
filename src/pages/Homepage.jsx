fede7803832af031ddd6443d88ffe46e5c28ce6d
SHresTho12 <talimul12bari@gmail.com>
2022-10-07 21:18:06 +0600

    query button condition fixed

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,51 +1,108 @@
 import {
- VStack, Stack,Image,Badge,chakra,Code,ChakraProvider,Heading,List,ListItem,OrderedList,Tag,Text,Box,HStack,IconButton,Spacer,useColorMode,useColorModeValue,Grid, GridItem,Button,ButtonGroup
-} from '@chakra-ui/react'
+  VStack,
+  Stack,
+  Image,
+  Badge,
+  chakra,
+  Code,
+  ChakraProvider,
+  Heading,
+  List,
+  ListItem,
+  OrderedList,
+  Tag,
+  Text,
+  Box,
+  HStack,
+  IconButton,
+  Spacer,
+  useColorMode,
+  useColorModeValue,
+  Grid,
+  GridItem,
+  Button,
+  ButtonGroup,
+} from "@chakra-ui/react";
 
-
-import React from 'react'
-import '@fontsource/raleway/400.css'
-import '@fontsource/open-sans/700.css'
-import '@fontsource/josefin-sans/700.css'
-import '@fontsource/teko/700.css'
-import theme from './theme'
-import Navlink from './HomePagelink'
-import { useAuth } from '../Context/AuthContext'
+import React from "react";
+import "@fontsource/raleway/400.css";
+import "@fontsource/open-sans/700.css";
+import "@fontsource/josefin-sans/700.css";
+import "@fontsource/teko/700.css";
+import theme from "./theme";
+import Navlink from "./HomePagelink";
+import { useAuth } from "../Context/AuthContext";
 
 // import iut from './iut.PNG';
 
-
-
-import { Layout } from '../components/Layout'
+import { Layout } from "../components/Layout";
 // import { Link } from 'react-router-dom'
-import { MdBuild , MdCall } from "react-icons/md"
-var Blur = require('react-blur');
-
-
+import { MdBuild, MdCall } from "react-icons/md";
+var Blur = require("react-blur");
 
 export default function Homepage() {
-  const { logout, currentUSer } = useAuth()
-
+  const { logout, currentUSer } = useAuth();
 
   return (
     <ChakraProvider theme={theme}>
-    
-    <Layout>
-    <VStack spacing={6} color="green.500">
-      <Heading >Welcome to IUT Edumate!</Heading>
- 
+      <Layout>
+        <VStack spacing={6} color="green.500">
+          <Heading>Welcome to IUT Edumate!</Heading>
 
+          <Stack direction="row" gap={0} spacing={6}>
+            {/* {currentUSer && <Navlink to='/profile' name='Profile' bgColor='white' colorScheme='green' />} */}
+            <Navlink
+              to="/register"
+              name="Academic Resources"
+              colorScheme="white"
+              _hover={{ bg: "purple.500" }}
+              bgColor="purple.400"
+              size="md"
+              height="70px"
+              width="200px"
+              border="2px"
+              borderColor="purple.300"
+            />
+          
+              <Navlink
+                to="/query"
+                name="Query section"
+                colorScheme="white"
+                _hover={{ bg: "green.500" }}
+                bgColor="green.400"
+                size="md"
+                height="70px"
+                width="200px"
+                border="2px"
+                borderColor="green.400"
+              />
+          
+            <Navlink
+              to="/register"
+              name="Project and Research"
+              colorScheme="white"
+              _hover={{ bg: "blue.500" }}
+              bgColor="blue.400"
+              size="md"
+              height="70px"
+              width="200px"
+              border="2px"
+              borderColor="blue.400"
+            />
+            <Navlink
+              to="/register"
+              name="Co-Curricular Activities"
+              colorScheme="white"
+              _hover={{ bg: "teal.500" }}
+              bgColor="teal.400"
+              size="md"
+              height="70px"
+              width="200px"
+              border="2px"
+              borderColor="teal.400"
+            />
 
-<Stack direction='row' gap ={0} spacing={6}>
-
-
-{/* {currentUSer && <Navlink to='/profile' name='Profile' bgColor='white' colorScheme='green' />} */}
-<Navlink to='/register' name='Academic Resources' colorScheme='white' _hover={{ bg: 'purple.500' }} bgColor='purple.400'  size="md" height="70px" width="200px" border="2px" borderColor="purple.300"/>
-{currentUSer && <Navlink to='/query' name='Query section' colorScheme='white'  _hover={{ bg: 'green.500' }} bgColor='green.400'  size="md" height="70px" width="200px" border="2px" borderColor="green.400"/>}
-<Navlink to='/register' name='Project and Research' colorScheme='white' _hover={{ bg: 'blue.500' }} bgColor='blue.400'  size="md" height="70px" width="200px" border="2px" borderColor="blue.400"/>
-<Navlink to='/register' name='Co-Curricular Activities' colorScheme='white'  _hover={{ bg: 'teal.500' }} bgColor='teal.400'  size="md" height="70px" width="200px" border="2px" borderColor="teal.400"/>
-
-{/* <Navlink to='/register' colorScheme='purple'   size="md" onClick={() => console.log("button clicked")}
+            {/* <Navlink to='/register' colorScheme='purple'   size="md" onClick={() => console.log("button clicked")}
   height="80px"
   width="225px"
   border="2px"
@@ -77,16 +134,15 @@ export default function Homepage() {
   borderColor="teal.500">
     Co-Curricular Activities
   </Button> */}
-
-
-</Stack>
-<Image src="/images/iut7.jpg" alt="s"  borderRadius="10" sx={{ filter: 'blur(0px)' }}/>
-
-</VStack>
-      
-    </Layout>
+          </Stack>
+          <Image
+            src="/images/iut7.jpg"
+            alt="s"
+            borderRadius="10"
+            sx={{ filter: "blur(0px)" }}
+          />
+        </VStack>
+      </Layout>
     </ChakraProvider>
-   
-  
-  )
+  );
 }
