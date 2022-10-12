00ad1b45210d236d77fa336896e5eb033d257f61
SHresTho12 <talimul12bari@gmail.com>
2022-10-13 01:01:30 +0600

    text editor addded

--- src/components/Query-Section/AskQuestion.js
+++ src/components/Query-Section/AskQuestion.js
@@ -3,15 +3,25 @@ import React from "react";
 import { Layout } from "../ProLayout";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
-
+import Tags from "./Tags";
+import "../../Css/Query-Section-Css/addquestion.css";
+import Editor from "./Editor";
 function AskQuestion() {
   return (
     <Layout>
-      <Heading align="center" m={4}>
-        Post a new question
-      </Heading>
-      <Box p={10}>
-        <VStack>
+      <Box
+        p={10}
+        border="1px"
+        align="center"
+        borderColor="teal"
+        borderRadius="10"
+        boxShadow="inner"
+        bgColor="teal.50"
+      >
+        <Heading align="center" m={2}>
+          Post a new question
+        </Heading>
+        <VStack align="left">
           <Box m={3}>
             <Heading my={2} size="md" align="left">
               Title
@@ -23,18 +33,22 @@ function AskQuestion() {
               htmlSize={100}
               size="lg"
               placeholder="Title of the question "
+              border="2px"
+              bgColor="teal.100"
             ></Input>
           </Box>
-          <Box>
+          <Box mt="2" alignItems="left">
             <Heading m={3} size="md" align="left">
               Description of the Question
             </Heading>
-            <ReactQuill theme="snow" />
+            {/* //<ReactQuill className="react-quill" theme="snow" /> */}
+            <Editor></Editor>
           </Box>
           <Box>
             <Heading m={3} size="md" align="left">
               Tags
             </Heading>
+            <Tags></Tags>
           </Box>
         </VStack>
       </Box>
