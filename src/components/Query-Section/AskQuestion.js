1f1a0ff12f375827ac346af0ce8a3f26217ecc4f
SHresTho12 <talimul12bari@gmail.com>
2022-10-12 23:09:19 +0600

    react quill added and question create page added

--- src/components/Query-Section/AskQuestion.js
+++ src/components/Query-Section/AskQuestion.js
@@ -1,16 +1,43 @@
-import { Box, VStack, Heading, Input } from "@chakra-ui/react";
+import { Box, VStack, Heading, Input, Badge } from "@chakra-ui/react";
 import React from "react";
 import { Layout } from "../ProLayout";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 function AskQuestion() {
   return (
     <Layout>
-      <VStack>
-        <Box>
-          <Heading m={4}>Title of the question</Heading>
-          <Input size="lg" placeholder="title"></Input>
-        </Box>
-      </VStack>
+      <Heading align="center" m={4}>
+        Post a new question
+      </Heading>
+      <Box p={10}>
+        <VStack>
+          <Box m={3}>
+            <Heading my={2} size="md" align="left">
+              Title
+            </Heading>
+            <Badge my={2} p={1} fontSize="sm" color="green" borderRadius={10}>
+              Be specific with the question and try to keep the title short
+            </Badge>
+            <Input
+              htmlSize={100}
+              size="lg"
+              placeholder="Title of the question "
+            ></Input>
+          </Box>
+          <Box>
+            <Heading m={3} size="md" align="left">
+              Description of the Question
+            </Heading>
+            <ReactQuill theme="snow" />
+          </Box>
+          <Box>
+            <Heading m={3} size="md" align="left">
+              Tags
+            </Heading>
+          </Box>
+        </VStack>
+      </Box>
     </Layout>
   );
 }
