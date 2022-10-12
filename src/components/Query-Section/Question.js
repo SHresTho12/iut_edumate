a2423e5d763848631635d06b2d190171d7e153e0
SHresTho12 <talimul12bari@gmail.com>
2022-10-12 20:27:55 +0600

    tags added

--- src/components/Query-Section/Question.js
+++ src/components/Query-Section/Question.js
@@ -1,25 +1,39 @@
-import { Box, ButtonGroup, Heading, VStack,Button } from '@chakra-ui/react'
-import React from 'react'
+import {
+  Box,
+  ButtonGroup,
+  Heading,
+  VStack,
+  Button,
+  HStack,
+  Tag,
+} from "@chakra-ui/react";
+import React from "react";
 
 function Question() {
   return (
-<VStack alignItems='right' borderY='1px' m='2'>
-<Heading size='lg'>
-    Why are there too much cats in iut?
-</Heading>                      
-<Box>
-    Because it is a cat haven.
-</Box>
-<ButtonGroup m='2'>
-    <Button colorScheme="teal" size="md">Comment</Button>
-    <Button colorScheme="teal" size="md">Upvote</Button>
-    <Button colorScheme="red" size="md">Downvote</Button>
-</ButtonGroup>
-<Box>
-    Top Answer
-</Box>
-</VStack>
-  )
+    <VStack alignItems="right" borderY="1px" m="2">
+      <Heading size="lg">Why are there too much cats in iut?</Heading>
+      <HStack>
+        <Tag>CSE</Tag>
+        <Tag>3-1</Tag>
+        <Tag>Software Security</Tag>
+        <Tag>CSE</Tag>
+      </HStack>
+      <Box>Because it is a cat haven.</Box>
+      <ButtonGroup m="2">
+        <Button colorScheme="teal" size="md">
+          Comment
+        </Button>
+        <Button colorScheme="teal" size="md">
+          Upvote
+        </Button>
+        <Button colorScheme="red" size="md">
+          Downvote
+        </Button>
+      </ButtonGroup>
+      <Box>Top Answer</Box>
+    </VStack>
+  );
 }
 
-export default Question
+export default Question;
