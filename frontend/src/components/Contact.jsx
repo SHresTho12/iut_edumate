c95b42bb0c0fcf21ba47cb59a128ed63ed470875
SHresTho12 <talimul12bari@gmail.com>
2022-10-14 20:07:15 +0600

    all merge conflicts resolved

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -14,7 +14,7 @@ import Phone from "../img/phone.png";
 import Email from "../img/email.png";
 import Address from "../img/address.png";
 import { useContext, useRef, useState } from "react";
-import emailjs from "emailjs-com";
+
 import { ThemeContext } from "../context";
 import { FilePondComponent } from "./filepond";
 
