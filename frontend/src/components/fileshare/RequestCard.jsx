89a6c87792984c3ac5b44902a606a7e5c96ef66b
MDMohaymenUlAnam <mohaymenulanamnihan@gmail.com>
2022-12-29 01:41:58 +0600

    some fixes

--- frontend/src/components/fileshare/RequestCard.jsx
+++ frontend/src/components/fileshare/RequestCard.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Box, Flex, Heading, Text,VStack,HStack,Button,ChakraProvider } from '@chakra-ui/react'
+import { Box, Flex, Heading, Text,Stack,VStack,HStack,Button,ChakraProvider } from '@chakra-ui/react'
 import theme from "../Query-Section/theme";
 function RequestCard({ request }) {
     console.log(request)
   return (
   
  <ChakraProvider theme={theme}>
+ <HStack>
       <VStack alignItems="right"  m="2" color="#00454A" shadow={'2xl'} margin='20px'>
-        <Box fontSize="x-large" bgColor="#affac9" borderRadius='5px' height={'7vh'} >
+        <Box fontSize="x-large" bgColor="#B7C7FC" borderRadius='5px' height={'7vh'} >
           <Link to={`/download?id=${request?.uuid}`} >{request?.title}</Link>
         </Box>
         <HStack>
@@ -26,8 +27,8 @@ function RequestCard({ request }) {
 
         
           <Button
-            bgColor="#affac9"
-            _hover={{ bg: "#73de98" }}
+            bgColor="#B7C7FC"
+            _hover={{ bg: "#9FB4FB" }}
             size="md"
             marginLeft="1vw"
             marginBottom={'40vh'}
@@ -38,8 +39,10 @@ function RequestCard({ request }) {
     }
     
           </Button>
+          
         
       </VStack>
+      </HStack>
     </ChakraProvider>
   )
 }
