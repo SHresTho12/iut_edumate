d5d1603fdc85ade8602423b5f49b89850d1ad279
MDMohaymenUlAnam <mohaymenulanamnihan@gmail.com>
2022-12-29 14:48:38 +0600

    chnages for models and router for project

--- backend/routers/Project.js
+++ backend/routers/Project.js
@@ -34,5 +34,83 @@ router.post("/", async (req, res) => {
         });
       });
   });
+  router.put("/interested/:id", async (req, res) => {
+    const id = req.params.id;
+    const user = req.body.user.uid;
+    const project = await projectDB.findById(id);
+    let interested = project.interested;
+    if (interested.includes(user)) {
+      res.status(400).send({
+        status: false,
+        message: "You have already added this note to your interested list",
+      });
+    } else {
+      interested.push(user);
+      await projectDB.findByIdAndUpdate(id, {
+        interested: interested,
+      });
+      res.status(200).send({
+        status: true,
+        message: "Project is added to your interested list",
+      });
+    }
+  });
+  
+  //get individual note by param id nad comments related to that note
+  router.get("/", async (req, res) => {
+    const error = {
+      message: "Error in retrieving projects",
+      error: "Bad request",
+    };
+  
+    projectDB
+      .aggregate([
+     
+  
+        {
+          $project: {
+            __v: 0,
+         
+          },
+        },
+      ])
+      .exec()
+      .then((projectDetails) => {
+        res.status(200).send(projectDetails);
+      })
+      .catch((e) => {
+        console.log("Error: ", e);
+        res.status(400).send(error);
+      });
+  });
+  
+  //detailed note info
+  router.get("/:id", async (req, res) => {
+    try {
+      projectDB
+        .aggregate([
+      
+          {
+            $project: {
+              __v: 0,
+            },
+          },
+        ])
+        .exec()
+        .then((projectDetails) => {
+          res.status(200).send(projectDetails);
+        })
+        .catch((e) => {
+          console.log("Error: ", e);
+          res.status(400).send(e);
+        });
+    } catch (err) {
+      console.log(err);
+      res.status(400).send({
+        message: "Note not found",
+      });
+    }
+  });
+
   
   module.exports = router;
